3ec250c4afc9aa4a2ea474771038474f8b78183b
codeb1ooded <meghaaggarwal493@gmail.com>
2018-02-24 11:05:46 +0530

    Navigation menu added to question page

--- static/js/nav_menu.js
+++ static/js/nav_menu.js
@@ -1,13 +1,3 @@
-// Sticky Header
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
-
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
     if ($('.main_h').hasClass('open-nav')) {
